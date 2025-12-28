6505bb2ac81ad3e485adb53a8c4e717846ebb694
Cyber_Mamriot <Cyber_Mamriot@CYBER-MAMRIOT>
2025-12-28 22:20:22 +0200

    done

--- GameConsole/Pages/SortHistoryGameByNameScreen.cs
+++ GameConsole/Pages/SortHistoryGameByNameScreen.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using GameConsole.Base;
+using GameConsole.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using GameConsole.Base;
-using GameConsole.Models;
+using System.Xml.Linq;
+using GameConsole;
 
 
 namespace GameConsole.Pages
@@ -17,12 +19,23 @@ namespace GameConsole.Pages
         public override void Show()
         {
             base.Show();
+            var names = ConsoleGame.user.AllScores.OrderByDescending(name => name.Name);
+            if (names != null)
+            {
+                int i = 1;
+                foreach (var name in names)
+                {
+                    //.Pastel(Color.FromArgb(255, 215, 0)
+                    // string write = i.ToString() + ". Score: " + score.Score + ", game: " + score.Name;
+                    Console.WriteLine(i.ToString() + ". Game: " + name.Name +  ", score: " + name.Score + ", date: " + name.Date);
+                    i++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("You didn't played any game yet.");
+            }
 
-
-            // להשלים כאן קוד
-            //
-            //
-            //
 
 
             Console.ForegroundColor = ConsoleColor.DarkBlue;
